9df16df9de7b20b13ca08db907a28829c1d17d6e
Bi2Nb9O3 <Bi2Nb9O3@bi2nb9o3.xyz>
2023-07-26 20:16:37 +0800

    Update adaptive navigation bar

--- index.php
+++ index.php
@@ -15,74 +15,60 @@ require_once "mysqlConnect.php";
 </head>
 
 <body>
-    <header>
-        <div>DMS</div>
-        <nav>
-            <ul class="nav_links">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </nav>
-        <a href="" class="cta" href="#"><button>Home</button></a>
-    </header>
-        <div id="main">
+    <iframe src="/nav/index.html" frameborder="0" style="height:48px;width:100vw;" id="main-nav" scrolling="no"></iframe>
+    <div id="main">
 
-            <div id="notSupportedBrowerAlert">
-                <h1 id="NSBA-title">警告:您正在使用不被支持的浏览器</h1>
-                <div id="NSBA-SupportedBrowsersList">请使用IE9+,Firefox3.5+,Chrome4+,Safari3+,Opera10+,iOS Mobile
-                    Safari4.2+
-                </div>
-                <div style="text-align: center;"><button id="NSBA-canel">忽略警告</button></div>
+        <div id="notSupportedBrowerAlert">
+            <h1 id="NSBA-title">警告:您正在使用不被支持的浏览器</h1>
+            <div id="NSBA-SupportedBrowsersList">请使用IE9+,Firefox3.5+,Chrome4+,Safari3+,Opera10+,iOS Mobile
+                Safari4.2+
             </div>
-            <h1 class="title">
-                DMS 宿舍管理系统
-            </h1>
-            <button onclick="location.href = 'login.php'">登录</button>
-            <button onclick="location.href = 'register.php'">注册</button>
-            <div id="rooms">
-                <div id="remain">
-                    <div style="text-align:center;margin-top:70px;">
-                        剩余房间:
-                        <div id="remainNum">
-                            <?php
-                            $con = mysqli_connect("localhost", "root", password: "123456", database: "dms-data");
-                            echo mysqli_num_rows(mysqli_query($con, "SELECT * FROM `rooms` WHERE status='empty'"));
-                            ?>
-                        </div>
+            <div style="text-align: center;"><button id="NSBA-canel">忽略警告</button></div>
+        </div>
+        <h1 class="title">
+            DMS 宿舍管理系统
+        </h1>
+        <div id="rooms">
+            <div id="remain">
+                <div style="text-align:center;margin-top:70px;">
+                    剩余房间:
+                    <div id="remainNum">
+                        <?php
+                        $con = mysqli_connect("localhost", "root", password: "123456", database: "dms-data");
+                        echo mysqli_num_rows(mysqli_query($con, "SELECT * FROM `rooms` WHERE status='empty'"));
+                        ?>
                     </div>
-
                 </div>
-                <div id="total">
-                    <div style="text-align:center;margin-top:70px;">
-                        房间总数:
-                        <div id="totalNum">
-                            <?php
-                            echo mysqli_num_rows(mysqli_query($con, "SELECT * FROM `rooms`"));
-                            ?>
-                        </div>
+
+            </div>
+            <div id="total">
+                <div style="text-align:center;margin-top:70px;">
+                    房间总数:
+                    <div id="totalNum">
+                        <?php
+                        echo mysqli_num_rows(mysqli_query($con, "SELECT * FROM `rooms`"));
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
-        <script>
-            var remain = document.getElementById("remainNum");
-            var total = document.getElementById("totalNum");
-            // console.log(remain.innerText+total.innerText);
-
-            if (remain.innerText / total.innerText >= 0.75) {
-                console.log(remain.innerText / total.innerText);
-                document.getElementById("remain").style.background = "rgb(186, 255, 181)";
-            } else if (remain.innerText / total.innerText >= 0.25) {
-                console.log(remain.innerText / total.innerText);
-                document.getElementById("remain").style.background = "rgb(255, 222, 181)"
-            } else {
-                console.log(remain.innerText / total.innerText);
-                document.getElementById("remain").style.background = "rgb(255, 181, 181)"
-            }
-        </script>
-        <script src="./js/isNotSupportedBrower.js"></script>
+    </div>
+    <script>
+        var remain = document.getElementById("remainNum");
+        var total = document.getElementById("totalNum");
+        // console.log(remain.innerText+total.innerText);
+        if (remain.innerText / total.innerText >= 0.75) {
+            console.log(remain.innerText / total.innerText);
+            document.getElementById("remain").style.background = "rgb(186, 255, 181)";
+        } else if (remain.innerText / total.innerText >= 0.25) {
+            console.log(remain.innerText / total.innerText);
+            document.getElementById("remain").style.background = "rgb(255, 222, 181)"
+        } else {
+            console.log(remain.innerText / total.innerText);
+            document.getElementById("remain").style.background = "rgb(255, 181, 181)"
+        }
+    </script>
+    <script src="./js/isNotSupportedBrower.js"></script>
 </body>
 
 </html>
