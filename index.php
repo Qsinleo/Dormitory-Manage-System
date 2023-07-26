58ffb162e6bca71ef90ddd9a45ba66274c090bc4
LeoMinistry <lylsnrc@126.com>
2023-07-26 15:32:53 +0800

    SC

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,17 +8,19 @@
     <link rel="stylesheet" href="./css/homepage.css">
     <link rel="stylesheet" href="./css/notSupportedBrowserAlert.css">
 </head>
+
 <body>
     <script src="./js/isNotSupportedBrower.js"></script>
-    <section id="main">
+    <div id="main">
         <div id="notSupportedBrowerAlert">
-            <h1  id="NSBA-title">警告:您正在使用不被支持的浏览器</h1>
+            <h1 id="NSBA-title">警告:您正在使用不被支持的浏览器</h1>
             <div id="NSBA-SupportedBrowsersList">请使用IE9+,Firefox3.5+,Chrome4+,Safari3+,Opera10+,iOS Mobile Safari4.2+</div>
             <button>忽略警告</button>
         </div>
-        <div class="h1">
+        <h1>
             DMS 宿舍管理系统
-        </div>
-    </section>
+        </h1>
+    </div>
 </body>
+
 </html>
