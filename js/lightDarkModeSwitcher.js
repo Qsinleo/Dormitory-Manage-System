3cc5e30b9c95f15d73c06c70e8a3efea15d8148b
Bi2Nb9O3 <Bi2Nb9O3@bi2nb9o3.xyz>
2023-08-17 16:44:40 +0800

    dark light mode

---
+++ js/lightDarkModeSwitcher.js
@@ -0,0 +1,27 @@
+const switcher=() => {
+    document.body.classList.toggle("dark");
+    if(body.classList.contains('dark')){
+        modeText.innerText="Light mode"
+    }else{
+        modeText.innerText="Dark mode"
+    }
+};
+const media = window.matchMedia("(prefers-color-scheme:dark)");
+
+// 判断是否为暗主题
+if (media.matchs) {
+  // 匹配到暗主题
+  switcher();
+} else {
+  // 没有匹配到暗主题
+  switcher();
+}
+
+// 上面操作只会在页面加载时才会生效，因此，需要给media添加事件监听器
+media.addEventListener("change", (e) => {
+  if (e.matches) {
+    switcher();
+  } else {
+    switcher();
+  }
+});
