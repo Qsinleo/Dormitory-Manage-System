dfc1a300eb2abf5a6e87c6ed010dc7f9c2d622bc
Bi2Nb9O3 <me@bi2nb9o2.xyz>
2023-09-30 19:19:59 +0800

    CSS

--- navpage.php
+++ navpage.php
@@ -5,130 +5,481 @@ function data_uri($contents, $mime)
 }
 if (!is_null($usertype)) {
 ?>
-
     <style>
-        /* YOUR CSS */
-        nav {
-            background-color: aquamarine;
-            border-radius: 5px;
-            margin: 5px;
-            padding: 5px;
-            display: inline-block;
-            width: calc(100% - 65px);
-            vertical-align: middle;
-        }
+        /* 字体库不一定要是我这个，甚至不用也可以 */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
 
-        .inline-title {
-            font-size: large;
-            font-weight: bold;
-        }
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+}
 
-        nav a {
-            text-decoration: none;
-            background-color: green;
-            color: whitesmoke;
-            margin: 3px;
-            padding: 3px;
-            border-radius: 5px;
-            transition-duration: .2s;
-        }
+/* 一些需要重复使用的样式 */
+:root {
+    /* 颜色 */
+    --body-color: rgb(139, 195, 255);
+    --sidebar-color: #fff;
+    --primary-color: #695cfe;
+    --primary-color-light: #f6f5ff;
+    --toggle-color: #ddd;
+    --text-color: #707070;
+    --second-body-color: rgb(176, 233, 255);
 
-        nav a:hover {
-            background-color: blueviolet;
-        }
+    /* 过渡效果 */
+    --tran-02: all 0.2s ease;
+    --tran-03: all 0.3s ease;
+}
 
-        nav a:active {
-            background-color: purple;
-        }
+body {
+    min-height: 100vh;
+    background-color: var(--body-color);
+    transition: var(--tran-03);
+}
 
-        .left-side {
-            display: inline-block;
-            text-align: left;
-            width: 60%;
-        }
+::selection {
+    background-color: var(--primary-color);
+    color: #fff;
+}
 
-        .right-side {
-            display: inline-block;
-            width: 30%;
-            margin-left: 8%;
-            text-align: right;
-        }
+/* 当body标签拥有dark类名的时候的样式 */
+body.dark {
+    --body-color: #1f82aa;
+    --sidebar-color: #242526;
+    --primary-color: #3a3b3c;
+    --primary-color-light: #3a3b3c;
+    --toggle-color: #fff;
+    --text-color: #ccc;
+}
 
-        .name {
-            font-size: large;
-            font-weight: bold;
-        }
+/* sidebar上的初始化样式 */
+.sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 13vw;
+    padding: 10px 14px;
+    background: var(--sidebar-color);
+    transition: var(--tran-03);
+    z-index: 100;
+}
 
-        .access-user {
-            font-size: small;
-            font-style: italic;
-        }
+.sidebar.close {
+    width: 4.5vw;
+}
 
-        .header {
-            width: 35px;
-            height: 35px;
-            border-radius: 50px;
-            border: 1px solid;
-            vertical-align: middle;
-        }
+.sidebar li {
+    height: 50px;
+    list-style: none;
+    display: flex;
+    align-items: center;
+    margin-top: 10px;
+}
+
+.sidebar header .image,
+.sidebar .icon {
+    min-width: 60px;
+    border-radius: 6px;
+}
+
+.sidebar header .image {
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+}
+
+.sidebar .icon {
+    min-width: 60px;
+    border-radius: 6px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+}
+
+.sidebar .text,
+.sidebar .icon {
+    color: var(--text-color);
+    transition: var(--tran-03);
+}
+
+.sidebar .text {
+    font-size: 17px;
+    font-weight: 500;
+    white-space: nowrap;
+    opacity: 1;
+}
+
+.sidebar.close .text {
+    opacity: 0;
+
+}
+
+/* header部分样式 */
+
+.sidebar header {
+    position: relative;
+}
+
+.sidebar header .image-text {
+    display: flex;
+    align-items: center;
+}
+
+.sidebar header .logo-text {
+    display: flex;
+    flex-direction: column;
+}
+
+header .image-text .name {
+    margin-top: 2px;
+    font-size: 18px;
+    font-weight: 600;
+}
+
+header .image-text .profession {
+    font-size: 16px;
+    margin-top: -2px;
+    display: block;
+}
+
+.sidebar header .image {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.sidebar header .image img {
+    width: 40px;
+    border-radius: 6px;
+}
+
+.sidebar header .toggle {
+    position: absolute;
+    top: 50%;
+    right: -25px;
+    transform: translateY(-50%) rotate(180deg);
+    height: 25px;
+    width: 25px;
+    background-color: var(--primary-color);
+    color: var(--sidebar-color);
+    display: flex;
+    border-radius: 50%;
+    align-items: center;
+    justify-content: center;
+    font-size: 22px;
+    cursor: pointer;
+    transition: var(--tran-03);
+}
+
+body.dark .sidebar header .toggle {
+    color: var(--text-color);
+}
+
+.sidebar.close .toggle {
+    transform: translateY(-50%) rotate(0deg);
+}
+
+.sidebar .menu {
+    margin-top: 40px;
+}
+
+.sidebar li.search-box {
+    border-radius: 6px;
+    background-color: var(--primary-color-light);
+    cursor: pointer;
+    transition: var(--tran-03);
+}
+
+.sidebar li.search-box input {
+    height: 100%;
+    width: 100%;
+    outline: none;
+    border: none;
+    background-color: var(--primary-color-light);
+    color: var(--text-color);
+    border-radius: 6px;
+    font-size: 17px;
+    font-weight: 500;
+    transition: var(--tran-03);
+}
+
+.sidebar li a {
+    list-style: none;
+    height: 100%;
+    background-color: transparent;
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    border-radius: 6px;
+    text-decoration: none;
+    transition: var(--tran-03);
+}
+
+.sidebar li a:hover {
+    background-color: var(--primary-color);
+}
+
+.sidebar li a:hover .icon,
+.sidebar li a:hover .text {
+    color: var(--sidebar-color);
+}
+
+body.dark .sidebar li a:hover .icon,
+body.dark .sidebar li a:hover .text {
+    color: var(--text-color);
+}
+
+.sidebar .menu-bar {
+    height: calc(100% - 100px);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    overflow-y: scroll;
+}
+
+.menu-bar::-webkit-scrollbar {
+    display: none;
+}
+
+.sidebar .menu-bar .mode {
+    border-radius: 6px;
+    background-color: var(--primary-color-light);
+    position: relative;
+    transition-timing-function: var(--tran-03);
+}
+
+.menu-bar .mode .sun-moon {
+    height: 50px;
+    width: 60px;
+}
+
+.mode .sun-moon i {
+    position: absolute;
+}
+
+.mode .sun-moon i.sun {
+    opacity: 0;
+}
+
+body.dark .mode .sun-moon i.sun {
+    opacity: 1;
+}
+
+body.dark .mode .sun-moon i.moon {
+    opacity: 0;
+}
+
+.menu-bar .bottom-content .toggle-switch {
+    position: absolute;
+    right: 0;
+    height: 100%;
+    min-width: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 6px;
+    cursor: pointer;
+}
+
+.toggle-switch .switch {
+    position: relative;
+    height: 22px;
+    width: 40px;
+    border-radius: 25px;
+    background-color: var(--toggle-color);
+    transition: var(--tran-03);
+}
+
+.switch::before {
+    content: "";
+    position: absolute;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    top: 50%;
+    left: 5px;
+    transform: translateY(-50%);
+    background-color: var(--sidebar-color);
+    transition: var(--tran-03);
+}
+
+body.dark .switch::before {
+    left: 20px;
+}
+
+.home {
+    position: absolute;
+    top: 0;
+    left: 250px;
+    height: 100vh;
+    width: calc(100% - 250px);
+    background-color: var(--body-color);
+    transition: var(--tran-03);
+    -webkit-transition: var(--tran-03);
+    -moz-transition: var(--tran-03);
+    -ms-transition: var(--tran-03);
+    -o-transition: var(--tran-03);
+}
+
+.home .text {
+    font-size: 30px;
+    font-weight: 500;
+    color: var(--text-color);
+    padding: 12px 60px;
+}
+
+.sidebar.close~.home {
+    left: 78px;
+    height: 100vh;
+    width: calc(100% - 78px);
+}
+
+body.dark .home .text {
+    color: var(--text-color);
+}
     </style>
-    <div class="top">
-        <nav>
-            <div class="left-side">
-                <span class="inline-title">导航</span>
-                <a href="index.php">首页</a>
-                <a href="manage.php">管理</a>
-                <a href="roomlist.php">房间列表</a>
-                <a href="userlist.php">用户列表</a>
-                <a href="accept.php">批准</a>
-                <a href="logout.php">退出登录</a>
+
+    <!-- Nav -->
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <img src="<?php echo (is_null($usertype) || $usertype == "inactived" || is_null($userinfo["header"])) ? "img/stuff.webp" : data_uri($userinfo["header"], "image/png"); ?>" alt="头像">
+                </span>
+                <div class="text logo-text">
+                    <span class="name"><?php echo is_null($usertype) ? "访客" : (($usertype == "inactived") ? "[未激活]" : "" . $userinfo["realname"]) ?></span>
+                    <span class="profession"><?php
+                                                switch ($usertype) {
+                                                    case 'inactived':
+                                                        echo "未激活";
+                                                        break;
+                                                    case 'staff':
+                                                        echo "员工";
+                                                        break;
+                                                    case 'admin':
+                                                        echo "管理员";
+                                                        break;
+                                                    case 'system-admin':
+                                                        echo "系统管理员";
+                                                        break;
+                                                    default:
+                                                        echo "访客";
+                                                        break;
+                                                }
+                                                ?></span>
+                </div>
             </div>
-            <div class="right-side">
-                <span class="name"><?php echo (($usertype == "inactived") ? "[未激活]" : "") . $userinfo["realname"] ?></span>
-                <span class="access-user"><?php
-                                            switch ($usertype) {
-                                                case 'inactived':
-                                                    echo "未激活";
-                                                    break;
-                                                case 'staff':
-                                                    echo "员工";
-                                                    break;
-                                                case 'admin':
-                                                    echo "管理员";
-                                                    break;
-                                                case 'system-admin':
-                                                    echo "系统管理员";
-                                                    break;
-                                                default:
-                                                    echo "访客";
-                                                    break;
-                                            }
-                                            ?></span>
+
+            <i class="bx bx-chevron-right toggle"></i>
+        </header>
+        <div class="menu-bar">
+            <div class="menu">
+                <li class="search-box">
+                    <i class="bx bx-search icon"></i>
+                    <input type="text" placeholder="Search...">
+                </li>
+
+                <ul class="menu-links">
+
+                    <li class="nav-link">
+                        <a href="index.php">
+                            <i class="bx bx-home-alt icon"></i>
+                            <span class="text nav-text">首页</span>
+                        </a>
+                    </li>
+                    <?php if ($usertype == "admin" || $usertype == "system-admin") { ?>
+                        <li class="nav-link">
+                            <a href="accept.php">
+                                <i class='bx bx-bar-chart-alt-2 icon'></i>
+                                <span class="text nav-text">批准</span>
+                            </a>
+                        </li>
+
+                        <li class="nav-link">
+                            <a href="roomlist.php">
+                                <i class='bx bx-bell icon'></i>
+                                <span class="text nav-text">房间列表</span>
+                            </a>
+                        </li>
+                    <?php } ?>
+                    <?php if (is_null($usertype)) { ?>
+                        <li class="nav-link">
+                            <a href="login.php">
+                                <i class='bx bx-pie-chart-alt icon'></i>
+                                <span class="text nav-text">登录</span>
+                            </a>
+                        </li>
+                        <li class="nav-link">
+                            <a href="register.php">
+                                <i class='bx bx-heart icon'></i>
+                                <span class="text nav-text">注册</span>
+                            </a>
+                        </li>
+                    <?php } ?>
+                    <!-- <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-wallet icon'></i>
+                            <span class="text nav-text">？？？</span>
+                        </a>
+                    </li> -->
+                </ul>
             </div>
-        </nav>
-        <img src="<?php echo !is_null($userinfo["header"]) ? data_uri($userinfo["header"], "image/jpeg") : "img/stuff.webp" ?>" class="header" />
-    </div>
+            <div class="bottom-content">
+                <?php if (!is_null($usertype)) { ?>
+                    <li class="nax-link">
+                        <a class="text nav-text" href='logout.php'><i class="bx bx-log-out icon"></i>Logout</a>
+                    </li>
+                <?php } ?>
+                <li class="mode">
+                    <div class="sun-moon">
+                        <i class="bx bx-moon icon moon"></i>
+                        <i class="bx bx-sun icon sun"></i>
+                    </div>
+                    <span class="mode-text text">Light Mode</span>
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+            </div>
+
+        </div>
+    </nav>
     <script>
-        const aTags = document.getElementsByTagName("nav")[0].getElementsByTagName("a");
-        switch (location.href.split("/")[location.href.split("/").length - 1]) {
-            case "manage.php":
-                aTags[1].style.backgroundColor = "orangered";
-                aTags[1].href = "#";
-                break;
-            case "roomlist.php":
-                aTags[2].style.backgroundColor = "orangered";
-                aTags[2].href = "#";
-                break;
-            case "userlist.php":
-                aTags[3].style.backgroundColor = "orangered";
-                aTags[3].href = "#";
-                break;
-            case "accept.php":
-                aTags[4].style.backgroundColor = "orangered";
-                aTags[4].href = "#";
-                break;
-            default:
-                break;
-        }
+        const body = document.querySelector("body");
+const sidebar = body.querySelector("nav");
+const toggle = body.querySelector(".toggle");
+const searchBtn = body.querySelector(".search-box");
+const modeSwitch = body.querySelector(".toggle-switch");
+const modeText = body.querySelector(".mode-text");
+function checkDarkMode() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Dark mode";
+  } else {
+    modeText.innerText = "Light mode";
+  }
+});
+
     </script>
 <?php } ?>
