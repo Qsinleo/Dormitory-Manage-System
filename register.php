c373bceb1d62682bdcceeca1cf95c9dc3065adaa
Bi2Nb9O3 <me@bi2nb9o2.xyz>
2023-10-01 10:18:48 +0800

    sc

--- register.php
+++ register.php
@@ -14,85 +14,42 @@ include_once "navpage.php";
 </head>
 
 <body>
-    <h1>注册</h1>
-    <form action="proceed.php" method="post">
-        <table>
-            <tr>
-                <td>真实姓名：</td>
-                <td><input name="realname" maxlength="8" required /></td>
-            </tr>
-            <tr>
-                <td>邮箱：</td>
-                <td><input name="email" type="email" maxlength="40" required /></td>
-                <td id="is-existed"></td>
-            </tr>
-            <tr>
-                <td>密码：</td>
-                <td><input name="password" type="password" required /></td>
-                <td id="is-long"></td>
-            </tr>
-            <tr>
-                <td>重新输入密码：</td>
-                <td><input id="password-retype" type="password" required /></td>
-                <td id="is-same"></td>
-            </tr>
-            <tr>
-                <td>工作部门：</td>
-                <td><input name="department" /></td>
-            </tr>
-            <tr>
-                <td>工号：</td>
-                <td><input name="workid" type="number" /></td>
-            </tr>
-            <tr>
-                <td>
-                    身份：
-                </td>
-                <td><select name="access" id="access">
+    <main>
+        <div class="wrapper fadeInDown">
+            <div id="formContent">
+                <!-- Tabs Titles -->
+                <h2 class="inactive underlineHover"> Sign In </h2>
+                <h2 class="active"> Sign Up </h2>
+
+                <!-- Icon -->
+                <div class="fadeIn first">
+                    <img src="./img/biLogo.svg" id="icon" alt="User Icon" />
+                </div>
+
+                <!-- Login Form -->
+                <form>
+                    <input type="text" name="realname" maxlength="8" placeholder="真名" />
+                    <input name="email" type="email" maxlength="40" placeholder="邮箱" />
+                    <input name="password" type="password" placeholder="密码" />
+                    <input id="password-retype" type="password" placeholder="重复密码" />
+                    <input type="text" name="department" placeholder="部门" />
+                    <input name="workid" type="number" placeholder="工号" />
+                    <select name="access" id="access">
                         <option value="staff" selected>员工</option>
                         <option value="admin">中级管理员</option>
                     </select>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">注册完成后需要<b>管理员</b>批准！</td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <input type="submit" value="注册" id="submit" />
-                </td>
-            </tr>
-            <td>
-            <td colspan="2" id="message"></td>
-            </td>
-        </table>
-        <input type="hidden" name="type" value="register" />
-    </form>
-    <div class="wrapper fadeInDown">
-        <div id="formContent">
-            <!-- Tabs Titles -->
-            <h2 class="inactive underlineHover"> Sign In </h2>
-            <h2 class="active"> Sign Up </h2>
+                    <input type="submit" class="fadeIn fourth" value="注册">
+                </form>
 
-            <!-- Icon -->
-            <div class="fadeIn first">
-                <img src="./img/biLogo.svg" id="icon" alt="User Icon" />
+                <!-- Remind Passowrd -->
+                <div id="formFooter">
+                    注册完成后需要<b>管理员</b>批准！
+                </div>
+
             </div>
-
-            <!-- Login Form -->
-            <form>
-                <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-                <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
-                <input type="submit" class="fadeIn fourth" value="Log In">
-            </form>
-
-            <!-- Remind Passowrd -->
-            <div id="formFooter">
-                <a class="underlineHover" href="#">Forgot Password?</a>
-            </div>
-
         </div>
-    </div>
+    </main>
+
 </body>
 <script src="js/required.js"></script>
 <script src="js/isExistedEmail.js"></script>
